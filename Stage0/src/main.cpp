7a34e922f4d7245ebba4a52ce4a4fcb242889924
samjwu <22262939+samjwu@users.noreply.github.com>
2024-08-10 10:21:11 -0600

    Add main.cpp

---
+++ Stage0/src/main.cpp
@@ -0,0 +1,31 @@
+// A simple program that computes the square root of a number
+
+// include the input/output stream library for printing
+#include <iostream>
+
+// include the header file for this project's custom math functions
+#include "mathfunctions.h"
+
+int main(int argc, char* argv[])
+{
+  // make sure the user provides an argument for the executable
+  if (argc < 2) {
+    // print usage guidelines
+    std::cout << "Please include a number as an argument." << std::endl;
+
+    std::cout << "Usage: " << argv[0] << " <number>" << std::endl;
+
+    return 1;
+  }
+
+  // convert input to double
+  const double inputValue = std::stod(argv[1]);
+
+  // call our custom square root function from the mathfunctions namespace
+  const double outputValue = mathfunctions::mysqrt(inputValue);
+
+  // print the result as output
+  std::cout << "The square root of " << inputValue << " is " << outputValue << std::endl;
+
+  return 0;
+}
